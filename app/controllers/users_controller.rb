9134193a05099d503759c0f0e1c113a226ce7eb0
JiaHuaCheng <jhchenge@gmail.com>
2021-04-06 10:49:57 -0500

    add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha blog #{@user.user_name}, you have successfully signup"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password)
+  end
+
+
 end
 
