d0f739dca9726c331b164fdbe5707ecf6719138e
JiaHuaCheng <jhchenge@gmail.com>
2021-04-06 01:51:36 -0500

    add password digest column and enable Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  
+  before_save { self.email = email.downcase }
+  
   has_many :articles
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :user_name, presence: true, length: {minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX}
   
+  has_secure_password
+
 end
